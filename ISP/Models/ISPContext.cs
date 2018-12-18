4c039167f72093f011f37de99c1f5aee05569600
giesmininkas <imantas.lukenskas@gmail.com>
2018-12-18 18:19:13 +0200

    dvjkbd

--- ISP/Models/ISPContext.cs
+++ ISP/Models/ISPContext.cs
@@ -35,7 +35,7 @@ namespace ISP
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseMySQL("Server=localhost;Database=ISP;User=root;Password=krokodilas");
+                optionsBuilder.UseMySQL("Server=localhost;Database=ISP;User=root;Password=password");
             }
         }
 
